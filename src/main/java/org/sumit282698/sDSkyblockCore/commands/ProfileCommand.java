d09819cdebc13f9362571a13d13b03f371843cb6
Sumit282698 <126954034+Sumit282698@users.noreply.github.com>
2026-06-05 12:28:30 +0530

    Some Updates in profile Command

--- src/main/java/org/sumit282698/sDSkyblockCore/commands/ProfileCommand.java
+++ src/main/java/org/sumit282698/sDSkyblockCore/commands/ProfileCommand.java
@@ -4,19 +4,20 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.jspecify.annotations.NonNull;
 import org.sumit282698.sDSkyblockCore.SDSkyblockCore;
-import org.sumit282698.sDSkyblockCore.menus.profilemenu;
+import org.sumit282698.sDSkyblockCore.menus.ProfileMenu;
 
-public class profilecommand implements CommandExecutor {
+public class ProfileCommand implements CommandExecutor {
 
     @Override
-    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+    public boolean onCommand(@NonNull CommandSender sender, @NonNull Command command, @NonNull String label, String @NonNull [] args) {
 
         if (!(sender instanceof Player player)) {
             sender.sendMessage("§cOnly players can use this command!");
             return true;
         }
-        profilemenu menu = new profilemenu(player);
+        ProfileMenu menu = new ProfileMenu(player);
         SDSkyblockCore.getInstance().getServer().getPluginManager().registerEvents(menu, SDSkyblockCore.getInstance());
         menu.open();
 
